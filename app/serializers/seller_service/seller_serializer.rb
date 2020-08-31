221efaffe9f9c370b8da4e7e8fa9452095ae1659
Arman <arman.zrb@gmail.com>
2020-08-31 17:16:40 +1000

    Remove user export from dash

--- app/serializers/seller_service/seller_serializer.rb
+++ app/serializers/seller_service/seller_serializer.rb
@@ -34,16 +34,6 @@ module SellerService
       }
     end
 
-    def bin2hex(data)
-      data.unpack('C*').map{ |b| "%02X" % b }.join('')
-    end
-
-    def blowfish(data)
-      cipher = OpenSSL::Cipher.new('bf-ecb').encrypt
-      cipher.key = Base64.decode64(ENV['ETENDERING_ENCRYPTION_KEY'])
-      cipher.update(data) << cipher.final
-    end
-
     def attributes(seller)
       profile = seller&.last_profile_version
       if seller
@@ -59,7 +49,6 @@ module SellerService
           lastProfileUpdate: profile&.updated_at&.strftime("%d %B %Y"),
           lastAccountUpdate: seller.latest_version&.updated_at&.strftime("%d %B %Y"),
           owner_id: seller.owner_id,
-          export_enc: @export_enc && seller.live? && bin2hex(blowfish(export_params(@user, seller.approved_version).to_json))
         })
       end
     end

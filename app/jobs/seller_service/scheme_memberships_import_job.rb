c839e8a82fd79c2263d6ad9bdaa8770d05a9a310
Arman <arman.zrb@gmail.com>
2020-09-09 23:38:39 +1000

    Data import from tenders

--- app/jobs/seller_service/scheme_memberships_import_job.rb
+++ app/jobs/seller_service/scheme_memberships_import_job.rb
@@ -1,6 +1,10 @@
 module SellerService
   class SchemeMembershipsImportJob < SharedModules::ApplicationJob
     def perform(document)
+      file = download_file(document)
+      xml_doc = Nokogiri::XML(File.open(file))
+      SellerService::PanelVendor.import(xml_doc)
+      document.update_attributes!(after_scan: nil)
     end
   end
 end

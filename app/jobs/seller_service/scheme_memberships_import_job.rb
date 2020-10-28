6a55a3216b6e81aed1e8f8fd24389d26d728036f
Arman Sarrafi <arman.zrb@gmail.com>
2020-10-29 10:17:20 +1100

    stop importing scheme memberships

--- app/jobs/seller_service/scheme_memberships_import_job.rb
+++ app/jobs/seller_service/scheme_memberships_import_job.rb
@@ -3,8 +3,9 @@ module SellerService
     def perform(document)
       file = download_file(document)
       xml_doc = Nokogiri::XML(File.open(file))
-      SellerService::PanelVendor.import(xml_doc)
-      document.update_attributes!(after_scan: nil)
+      # Todo: the following lines are commented to stop import of membership till we revisit
+      # SellerService::PanelVendor.import(xml_doc)
+      # document.update_attributes!(after_scan: nil)
     end
   end
 end

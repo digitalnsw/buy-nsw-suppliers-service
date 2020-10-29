6315a7bb0b6a97adf615f18792e77c58855f4eb3
Arman Sarrafi <arman.zrb@gmail.com>
2020-10-29 11:33:27 +1100

    enavle pv import

--- app/jobs/seller_service/scheme_memberships_import_job.rb
+++ app/jobs/seller_service/scheme_memberships_import_job.rb
@@ -4,8 +4,8 @@ module SellerService
       file = download_file(document)
       xml_doc = Nokogiri::XML(File.open(file))
       # Todo: the following lines are commented to stop import of membership till we revisit
-      # SellerService::PanelVendor.import(xml_doc)
-      # document.update_attributes!(after_scan: nil)
+      SellerService::PanelVendor.import(xml_doc)
+      document.update_attributes!(after_scan: nil)
     end
   end
 end
